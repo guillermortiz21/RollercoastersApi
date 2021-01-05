fc9dc5e5b5898ef935d15128f6d2182bd20e3107
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-04 23:16:40 -0600

    Completing dbActions Class

--- utils/DbActions.js
+++ utils/DbActions.js
@@ -3,6 +3,21 @@ class DbActions{
    const result = await model.findAll(options);
    return result;
   }
+
+  async insert(model, data){
+    const result = await model.create(data);
+    return result;
+  }
+
+  async update(model, data, options){
+    const result = await model.update(data, options);
+    return result;
+  }
+
+  async delete(model, options){
+    const result = await model.destroy(options);
+    return result;
+  }
 }
 
 const dbActions = new DbActions();
